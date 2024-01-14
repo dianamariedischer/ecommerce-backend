b645b1bd8cd2aa1786f2dc9910242f85bf9663f5
Diana Discher <dianadischer@dianas-mbp.myfiosgateway.com>
2024-01-14 18:47:10 -0500

    Updated models to not allow for null in specific instances and added certain validations

--- models/Product.js
+++ models/Product.js
@@ -16,13 +16,23 @@ Product.init(
       autoIncrement: true,
     },
     product_name: {
-      type: DataTypes.STRING
+      type: DataTypes.STRING,
+      allowNull: false,
     },
     price: {
-      type: DataTypes.FLOAT
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      validate: {
+        isDecimal: true,
+      },
     },
     stock: {
-      type: DataTypes.INTEGER
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+      validate: {
+        isNumeric: true,
+      },
     },
     category_id: {
       type: DataTypes.INTEGER,
